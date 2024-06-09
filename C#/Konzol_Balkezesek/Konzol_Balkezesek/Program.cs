9890498998ebf9ee1cf3e6d8c71c4a2125b80ce4
raidoon <71091048+raidoon@users.noreply.github.com>
2024-06-09 15:21:31 +0200

    "balkezesek" console app

---
+++ C#/Konzol_Balkezesek/Konzol_Balkezesek/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Konzol_Balkezesek
+{
+    internal class Program
+    {
+        public static List<adatsor> adatok = new List<adatsor>();
+        static void Main(string[] args)
+        {
+            string[] f = File.ReadAllLines("balkezesek.csv", Encoding.UTF8);
+            foreach (var a in f.Skip(1)) adatok.Add(new adatsor(a));
+
+            Console.WriteLine($"3.feladat: {adatok.Count()}");
+
+            Console.WriteLine("4.feladat:");
+            adatok.Where(x => x.utolso.Year.Equals(1999)
+            && x.utolso.Month.Equals(10)).ToList()
+            .ForEach(x=>Console.WriteLine($"\t{x.nev} {Math.Round(x.magassag * 2.54,1)}"));
+
+            Console.WriteLine($"5.feladat:");
+            bool ok = false;
+            Console.WriteLine("Kérek egy 1990 és 1999 közötti évszámot!");
+            var keresem = Console.ReadLine();
+            if (int.Parse(keresem) >= 1990 && int.Parse(keresem) <= 1999) ok = true;
+            else ok = false;
+            while (!ok)
+            {
+                Console.WriteLine("Hibás adat, kérek egy 1990 és 1999 közötti évszámot!");
+                var evszam = Console.ReadLine();
+                keresem = evszam;
+                if (int.Parse(evszam) >= 1990 && int.Parse(evszam) <= 1999) ok = true;
+            }
+
+            var evszamLista = adatok.Where(x => x.elso.Year <= int.Parse(keresem)
+            && x.utolso.Year >= int.Parse(keresem)).ToList();
+
+            Console.WriteLine($"6.feladat: {Math.Round(evszamLista.Average(x => x.suly),2)} font");
+            
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C#/Konzol_Balkezesek/Konzol_Balkezesek/adatsor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Konzol_Balkezesek
+{
+    internal class adatsor
+    {
+        /*név;első;utolsó;súly;magasság
+        Jim Abbott;1989-04-08;1999-07-21;200;75
+        Kyle Abbott;1991-09-10;1996-08-24;200;76*/
+
+        public string nev { get; set; }
+        public DateTime elso { get; set; }
+        public DateTime utolso { get; set;}
+        public int suly { get; set; }
+        public int magassag { get; set; } //inchben van
+        public adatsor(string sor)
+        {
+            string[] sz = sor.Split(';');
+            nev = sz[0];
+            elso = Convert.ToDateTime(sz[1]);
+            utolso = Convert.ToDateTime(sz[2]);
+            suly = int.Parse(sz[3]);
+            magassag = int.Parse(sz[4]);
+        }
+    }
+}
