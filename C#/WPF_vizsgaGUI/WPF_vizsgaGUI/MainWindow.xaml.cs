00e1736683c38f1495c9f07ae0075ace9ca019e1
raidoon <71091048+raidoon@users.noreply.github.com>
2024-06-11 21:46:38 +0200

    first WPF app (.NET framework)

---
+++ C#/WPF_vizsgaGUI/WPF_vizsgaGUI/MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.IO;
+
+namespace WPF_vizsgaGUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<adatsor> adatok = new List<adatsor> ();
+        string[] tantargyakLista = new string[8] { "IT és hálózatok írásbeli","Programozás írásbeli",
+            "Hálózatok A modul","Hálózatok B modul","Hálózatok C modul","Hálózatok D modul",
+            "Szóbeli angol","Szóbeli IT" };
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            adatokbetoltese();
+        }
+
+        private void adatokbetoltese()
+        {
+            adatok.Clear();
+            listbox.Items.Clear();
+            datagrid.ItemsSource = null;
+            string[] f = File.ReadAllLines("adatok.txt",Encoding.UTF8);
+            foreach (var a in f) adatok.Add(new adatsor(a));
+            datagrid.ItemsSource=adatok;
+            lberedmenyek.Content = $"összesen {adatok.Count()} diák vett részt a vizsgán";
+            lberedmenyek.Content += $"\nsikeres vizsgát tett tanulók száma: " +
+                $"{adatok.Where(x => x.Minosites != "elégtelen").Count()}";
+            //6.Hozza létre a vizsgaredmenyek.txt állományt!
+            //Írja ki az összes sikeres vizsga végeredményét! 
+            var eredmenyekLista = adatok.Where(x => x.Minosites != "elégtelen")
+                .Select(x => $"{x.Nev}\t{x.Eredmeny}\t{x.Minosites}");
+            File.WriteAllLines("vizsgaeredmenyek.txt", eredmenyekLista);
+        }
+
+        private void tbkeresetttanulo_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (tbkeresetttanulo.Text != "")
+            {
+                string keresett = tbkeresetttanulo.Text.ToLower();
+                var talalat = adatok.FirstOrDefault(x => x.Nev.ToLower().Contains(keresett));
+                if (talalat != null)
+                {
+                    double legjobbEredmeny = talalat.Vizsgaeredmenyek.Max();
+                    int legjobbtantargyIndexe = Array.IndexOf(talalat.Vizsgaeredmenyek, legjobbEredmeny);
+                    string legjobbTantargy = tantargyakLista[legjobbtantargyIndexe];
+                    
+                    double leggyengebbEredmeny = talalat.Vizsgaeredmenyek.Min();
+                    int leggyengebbTantargyIndexe = Array.IndexOf(talalat.Vizsgaeredmenyek, leggyengebbEredmeny);
+                    string leggyengebbTantargy = tantargyakLista[leggyengebbTantargyIndexe];
+
+                    string sikeresseg = talalat.Minosites != "elégtelen" ? "sikeres" : "nem sikeres";
+                    lbkereseseredmenye.Content = $"{talalat.Nev}\nlegjobb eredménye: {legjobbEredmeny} ({legjobbTantargy})" +
+                        $"\nleggyengébb eredménye: {leggyengebbEredmeny} {leggyengebbTantargy}" +
+                        $"\nvizsgája {sikeresseg}.";
+                }
+                else lbkereseseredmenye.Content = "A keresett tanuló nem található a listában";
+            }
+            else lbkereseseredmenye.Content = "Keresés eredménye:";
+        }
+
+        private void datagrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            listbox.Items.Clear();
+            if (datagrid.SelectedItem != null)
+            {
+                adatsor kivalasztottsor = (adatsor)datagrid.SelectedItem;
+                listbox.Items.Add($"{kivalasztottsor.Nev} eredményei:");
+                for (int i = 0; i < tantargyakLista.Length; i++)
+                {
+                    listbox.Items.Add($"{tantargyakLista[i]} " +
+                        $"eredménye: {kivalasztottsor.Vizsgaeredmenyek[i].ToString()}");
+                }
+            }
+            else listbox.Items.Clear();
+        }
+    }
+}
---
+++ C#/WPF_vizsgaGUI/WPF_vizsgaGUI/adatsor.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_vizsgaGUI
+{
+    internal class adatsor
+    {
+        public string Nev {  get; set; }
+        public string Minosites { get; set; }
+        public double Eredmeny { get; set; }
+        public double[] Vizsgaeredmenyek { get; set; }
+
+        public adatsor(string sor)
+        {
+            string[] sz = sor.Split(';');
+            Nev = sz[0];
+            Vizsgaeredmenyek = new double[8];
+            for (int i = 0; i < Vizsgaeredmenyek.Length; i++)
+            {
+                Vizsgaeredmenyek[i] = double.Parse(sz[i + 1]) * 100;
+            }
+            double atlag = Math.Round(Vizsgaeredmenyek.Average(),0);
+            Eredmeny= atlag;
+            if (Vizsgaeredmenyek.Min() <= 50) Minosites = "elégtelen";
+            else
+            {
+                if (atlag >= 51 && atlag <= 60) Minosites = "elégséges";
+                if (atlag >= 61 && atlag <= 70) Minosites = "közepes";
+                if (atlag >= 71 && atlag <= 80) Minosites = "jó";
+                if (atlag >= 81) Minosites = "jeles";
+            }
+        }
+    }
+}
