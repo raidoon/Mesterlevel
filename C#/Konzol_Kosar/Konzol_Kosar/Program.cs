13dec9cbcc80ae25f1cd539eb7ab33b853790a6d
raidoon <71091048+raidoon@users.noreply.github.com>
2024-06-11 21:43:30 +0200

    Added another console app

---
+++ C#/Konzol_Kosar/Konzol_Kosar/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Konzol_Kosar
+{
+    internal class Program
+    {
+        public static List<kosarAdatok> adatok = new List<kosarAdatok>();
+        static void Main(string[] args)
+        {
+            string[] f = File.ReadAllLines("eredmenyek.csv", Encoding.UTF8);
+            foreach (var a in f.Skip(1)) adatok.Add(new kosarAdatok(a));
+            Console.WriteLine($"3.feladat: Real Madrid: Hazai: {adatok.Where(x=>x.Hazai=="Real Madrid").Count()}, " +
+                $" Idegen: {adatok.Where(x => x.Idegen == "Real Madrid").Count()}");
+            string dontetlen = "";
+            foreach (var a in adatok)
+            {
+                if(a.HazaiPont == a.IdegenPont) dontetlen = "igen";
+                else dontetlen = "nem";
+            }
+            Console.WriteLine($"4.feladat: Volt döntetlen? {dontetlen}");
+            var barcelona = adatok.Where(x => x.Hazai.ToLower().Contains("barcelona")).First();
+            Console.WriteLine($"5.feladat: barcelonai csapat neve: {barcelona.Hazai}");
+            Console.WriteLine("6.feladat:");
+            DateTime KeresettIdopont = Convert.ToDateTime("2004.11.21");
+            adatok.Where(x => x.Idopont.Equals(KeresettIdopont)).ToList()
+                .ForEach(x => Console.WriteLine($"\t{x.Hazai} - {x.Idegen} ({x.HazaiPont}:{x.IdegenPont})"));
+            Console.WriteLine($"7.feladat:");
+            adatok.GroupBy(x => x.Helyszin).Where(x => x.Count() > 20).ToList()
+                .ForEach(x => Console.WriteLine($"\t{x.Key}: {x.Count()}"));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C#/Konzol_Kosar/Konzol_Kosar/kosarAdatok.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Konzol_Kosar
+{
+    internal class kosarAdatok
+    {
+        //hazai;idegen;hazai_pont;idegen_pont;helyszín;időpont
+        //7up Joventut; Adecco Estudiantes;81;73;Palacio Mun.De Deportes De Badalona;2005-04-03
+        public string Hazai { get; set; }
+        public string Idegen { get; set; }
+        public int HazaiPont { get; set; }
+        public int IdegenPont { get; set; }
+        public string Helyszin { get; set; }
+        public DateTime Idopont { get; set; }
+        public kosarAdatok(string sor)
+        {
+            string[] sz = sor.Split(';');
+            Hazai = sz[0];
+            Idegen = sz[1];
+            HazaiPont = int.Parse(sz[2]);
+            IdegenPont = int.Parse(sz[3]);
+            Helyszin = sz[4];
+            Idopont = Convert.ToDateTime(sz[5]);
+        }
+    }
+}
