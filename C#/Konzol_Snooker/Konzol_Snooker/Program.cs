7f4976d1e28d849abd7a60cdfa27bdeef3ae53d5
raidoon <71091048+raidoon@users.noreply.github.com>
2024-06-12 14:48:14 +0200

    Console app "snooker" feladat

---
+++ C#/Konzol_Snooker/Konzol_Snooker/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Konzol_Snooker
+{
+    internal class Program
+    {
+        public static List<adatsor> adatok = new List<adatsor>();
+        static void Main(string[] args)
+        {
+            string[] f = File.ReadAllLines("snooker.txt", Encoding.UTF8);
+            foreach (var a in f.Skip(1)) adatok.Add(new adatsor(a));
+            Console.WriteLine($"3.feladat: A világranglistán {adatok.Count()} versenyző szerepel");
+            
+            Console.WriteLine($"4.feladat: A versenyzők átlagosan {adatok.Average(x=>x.nyeremeny):### ###.##} fontot kerestek");
+
+            Console.WriteLine($"5.feladat: A legjobban kereső Kínai versenyző:");
+            var legjobbankereso = adatok.Where(x => x.orszag == "Kína").OrderByDescending(x => x.nyeremeny).First();
+
+            Console.WriteLine($"\tHelyezés: {legjobbankereso.helyezes}" +
+                $"\n\tNév: {legjobbankereso.nev}" +
+                $"\n\tOrszág: {legjobbankereso.orszag}" +
+                $"\n\tNyeremény összege: {legjobbankereso.nyeremeny * 380:### ### ###} Ft");
+
+            bool vanNorvegVersenyzo = adatok.Any(x => x.orszag == "Norvégia");
+            if(vanNorvegVersenyzo)
+            {
+                int norvegVersenyzoIndexe = adatok.FindIndex(x => x.orszag == "Norvégia") + 1;
+                Console.WriteLine($"6.feladat: Van norvég versenyző a listában. Az első norvég versenyző az " +
+                    $"adatok között a(z) {norvegVersenyzoIndexe}. sorban található.");
+            }
+            else Console.WriteLine("6.feladat: Nincs norvég versenyző a listában.");
+
+            Console.WriteLine("7.feladat: Statisztika");
+            adatok.GroupBy(x => x.orszag).Where(x=>x.Count()>4).ToList()
+                .ForEach(x=>Console.WriteLine($"\t{x.Key} - {x.Count()} fő"));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C#/Konzol_Snooker/Konzol_Snooker/adatsor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Konzol_Snooker
+{
+    internal class adatsor
+    {
+        public int helyezes { get; set; }
+        public string nev {  get; set; }
+        public string orszag { get; set; }
+        public double nyeremeny { get; set; }
+        public adatsor(string sor)
+        {
+            string[] sz = sor.Split(';');
+            helyezes = int.Parse(sz[0]);
+            nev = sz[1];
+            orszag = sz[2];
+            nyeremeny = double.Parse(sz[3]);
+        }
+    }
+}
