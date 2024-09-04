51e52b814d3786dd1db0e1f345bd533d47b90e94
raidoon <thomaselphin@gmail.com>
2024-09-04 13:40:16 +0200

    primszamok

---
+++ C#/oktato_program/oktato_program/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oktato_program
+{
+    internal class Program
+    {
+        public static int valasz = 0, a = 0, b = 0, muvelet = 0;
+        public static string muveletjel = "";
+        static void Main(string[] args)
+        {
+            //mennyi [véletlen szám x] + [véletlen szám y] = [kérje be a választ] 10x csinálja meg
+            // plusz és minusz is véletlen legyen
+            //max 20ig legyenek a véletlen számok
+            int eredmeny = 0, pontszam = 0;
+
+            int pluszminusz = 1;
+            for (int i = 0; i < 10; i++) 
+            {
+                if (pluszminusz%2==0) //osszeadas
+                {
+                    pluszminusz--;
+
+                }
+                else //kivonas
+                {
+                    pluszminusz++;
+                }
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C#/primszamok/primszamok/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace primszamok
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int szam = 0;
+            Console.Write("Meddig írjam ki a prímszámokat: ");
+            szam = int.Parse(Console.ReadLine());
+            Console.WriteLine($"A prímszámok {szam}-ig:");
+            for (int i = 2; i < szam; i++)
+            {
+                bool prim = true;
+                for(int j = 2; j < i && prim; j++)
+                {
+                    if (i % j == 0) prim = false;
+                }
+                if(prim) Console.Write($"{i}, ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
