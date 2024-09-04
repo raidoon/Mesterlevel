c3561f71081d8744a42b85df30c4d258d81eb398
raidoon <thomaselphin@gmail.com>
2024-09-04 11:43:59 +0200

    wpf

--- C#/Ismetles_Console_WPF/Ismetles_WPF/MainWindow.xaml.cs
+++ C#/Ismetles_Console_WPF/Ismetles_WPF/MainWindow.xaml.cs
@@ -21,17 +21,59 @@ namespace Ismetles_WPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        int telekszelesseg = 0, telekhossz = 0, kerulet = 0;
+        bool okhossz = false, okszelesseg = false;
         public MainWindow()
         {
-            if (tb_telekszelesseg.Text!=null && tb_telekhossz.Text != null)
-            {
-                int telekszelesseg = Convert.ToInt32(tb_telekszelesseg.Text);
-            }
+           InitializeComponent();
         }
 
+        private void tb_telekszelesseg_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            tb_kerulet.Text = "";
+        }
+
+        private void tb_telekhossz_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            tb_kerulet.Text = "";
+        }
         private void bt_szamol_Click(object sender, RoutedEventArgs e)
         {
+            tb_kerulet.Text = "";
+            if (tb_telekhossz.Text != "" && tb_telekszelesseg.Text != "")
+            {
+                okszelesseg = ellenoriz(lb_telekszelesseg, tb_telekszelesseg);
+                okhossz = ellenoriz(lb_telekhossz, tb_telekhossz);
+                if (okszelesseg && okhossz)
+                {
+                    kerulet = 2 * (int.Parse(tb_telekszelesseg.Text) + int.Parse(tb_telekhossz.Text));
+                    tb_kerulet.Text = $"{kerulet} méter";
+                }
+            }
+            else MessageBox.Show("Mindkét adatot meg kell adni!");
+        }
 
+        private bool ellenoriz(Label label, TextBox textbox)
+        {
+            bool ok = false;
+            int szam = 0;
+                try
+                {
+                    ok = false;
+                    szam = int.Parse(textbox.Text);
+                    if (szam <= 0)
+                    {
+                        ok = false;
+                        MessageBox.Show($"{label.Content} nem lehet - vagy 0");
+                    }
+                    else ok = true;
+                }
+                catch
+                {
+                    ok = false;
+                MessageBox.Show("Nem számot adtál meg!");
+                }
+            return ok;
         }
     }
 }
