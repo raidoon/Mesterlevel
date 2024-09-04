7e00399dc1f9a427940b39323f906b4421ff772d
raidoon <thomaselphin@gmail.com>
2024-09-04 12:42:27 +0200

    telefonszam console + wpf

---
+++ C#/telefon_Console_WPF/telefon_Console_WPF/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace telefon_Console_WPF
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleKeyInfo lenyomott_gomb = new ConsoleKeyInfo();
+            string telefonszam = ""; //begépelt értékek
+            string szamok = "0123456789"; //érvényes karakterek
+            int db = 0;
+            do
+            {
+                db = 0;
+                telefonszam = "";
+                Console.Write("Kérem a telefonszámot (a 06 után!): ");
+                do
+                {
+                    lenyomott_gomb=Console.ReadKey(true);
+                    if (szamok.Contains(lenyomott_gomb.KeyChar))
+                    {
+                        db++;
+                        Console.Write($"{lenyomott_gomb.KeyChar}");
+                        telefonszam += lenyomott_gomb.KeyChar;
+                    }
+                }
+                while (db < 9);
+                Console.WriteLine($"\nA megadott telefonszám: 06{telefonszam}");
+                Console.WriteLine("Kilépés ESC billentyűre!");
+                lenyomott_gomb =Console.ReadKey(true); //ha volt billentyű leütés, akkor az a változóba került
+            }
+            while (lenyomott_gomb.Key != ConsoleKey.Escape); //esc billentyűre fog kilépni
+        }
+    }
+}
---
+++ C#/telefon_Console_WPF/telefonszam_WPF/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace telefonszam_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        bool ok=false;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void telefonszam_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            ok = false;
+            string telszam = telefonszam.Text;
+            string szamok = "0123456789";
+            int db = 0;
+            if (telszam.Length == 9)
+            {
+                for (int i = 0; i < telszam.Length; i++)
+                {
+                    if (szamok.Contains(telszam[i])) db++;
+                }
+                if (db == telszam.Length) ok = true;
+            }
+            if (ok) 
+            {
+                telefonszam.Background=Brushes.Green;
+                telefonszam.Foreground=Brushes.White;
+            }
+            else
+            {
+                telefonszam.Background = Brushes.Red;
+                telefonszam.Foreground = Brushes.White;
+            }
+        }
+    }
+}
