76158965987c2c0be4db671adaec736604c53eeb
raidoon <thomaselphin@gmail.com>
2024-09-13 13:14:41 +0200

    console app nobel

---
+++ C#/ConsoleApp_Nobel/ConsoleApp_Nobel/Nobel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_Nobel
+{
+    public class Nobel
+    {
+        //év;típua;keresztnév;vezetéknév
+        public int ev { get; set; }
+        public string tipus { get; set; }
+        public string keresztNev { get; set; }
+        public string vezetekNev { get; set; }
+
+        public Nobel(string sor)
+        {
+            string[] n= sor.Split(';');
+            ev = int.Parse(n[0]);
+            tipus = n[1];
+            keresztNev = n[2];
+            vezetekNev = n[3];
+        }
+    }
+}
---
+++ C#/ConsoleApp_Nobel/ConsoleApp_Nobel/class_maskeppen.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_Nobel
+{
+    internal class class_maskeppen
+    {
+        public int ev;
+        public string tipus;
+        public string keresztNev;
+        public string vezetekNev;
+
+        public class_maskeppen(int ev, string tipus, string keresztNev, string vezetekNev)
+        {
+            this.ev = ev;
+            this.tipus = tipus;
+            this.keresztNev = keresztNev;
+            this.vezetekNev = vezetekNev;
+        }
+    }
+}
