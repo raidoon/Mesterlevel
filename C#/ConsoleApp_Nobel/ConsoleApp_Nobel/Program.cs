75302f7c66fd04f173a31598034d4683eaa4201f
raidoon <thomaselphin@gmail.com>
2024-09-16 08:14:43 +0200

    nobel console app update

--- C#/ConsoleApp_Nobel/ConsoleApp_Nobel/Program.cs
+++ C#/ConsoleApp_Nobel/ConsoleApp_Nobel/Program.cs
@@ -13,14 +13,31 @@ namespace ConsoleApp_Nobel
             List<Nobel> listaNobel = new List<Nobel>();
             string[] fajl = File.ReadAllLines("nobel.csv");
             foreach (string item in fajl.Skip(1)) listaNobel.Add(new Nobel(item));
-            foreach (var item in listaNobel) if (item.keresztNev == "Arthur B." && item.vezetekNev == "McDonald") Console.WriteLine($"3.feladat: {item.tipus}"); //Arthur B. McDonald milyen típusú díjat kapott?
-            foreach (var item in listaNobel) if (item.ev == 2017 && item.tipus == "irodalmi") Console.WriteLine($"4.feladat: {item.keresztNev + " " + item.vezetekNev}"); //ki kapott 2017-ben irodalmi nobel-díjat
+            //Arthur B. McDonald milyen típusú díjat kapott?
+            foreach (var item in listaNobel) if (item.keresztNev == "Arthur B." && item.vezetekNev == "McDonald") Console.WriteLine($"3.feladat: {item.tipus}");
+            //ki kapott 2017-ben irodalmi nobel-díjat
+            foreach (var item in listaNobel) if (item.ev == 2017 && item.tipus == "irodalmi") Console.WriteLine($"4.feladat: {item.keresztNev + " " + item.vezetekNev}"); 
+            //mely szervezetek kaptak béke nobel-díjat 1990-től napjainkig
             Console.WriteLine("5.feladat:");
-            foreach (var item in listaNobel) if (item.vezetekNev == "" && item.tipus == "béke" && item.ev >= 1990) Console.WriteLine($"\t{item.ev}: {item.keresztNev}");//mely szervezetek kaptak béke nobel-díjat 1990-től napjainkig
+            foreach (var item in listaNobel) if (item.vezetekNev == "" && item.tipus == "béke" && item.ev >= 1990) Console.WriteLine($"\t{item.ev}: {item.keresztNev}");
+            //a curie család több tagja is kapott díjat. Határozza meg és írja ki a képernyőre minta szerint, hogy melyik évben a család melyik tagja milyen díjat kapott
             Console.WriteLine("6.feladat:");
-            //foreach (var item in listaNobel) Console.WriteLine($"\t{item.ev}: {item.keresztNev}({item.tipus})");//
+            foreach (var item in listaNobel) if(item.vezetekNev.Contains("Curie")) Console.WriteLine($"\t{item.ev}: {item.keresztNev} {item.vezetekNev}({item.tipus})");
+            //melyik típusú díjból hány darabot osztottak ki a nobel-díj történelme folyamán
             Console.WriteLine("7.feladat:");
-            //foreach(var item in listaNobel) Console.WriteLine($"\t{item.tipus}\t db)"); //melyik típusú díjból hány darabot osztottak ki a nobel-díj történelme folyamán
+            List<string> tipusok = new List<string>();
+            
+            foreach(var item in listaNobel)
+            {
+                if (tipusok.Contains(item.tipus)) { }
+                else
+                {
+                     tipusok.Add(item.tipus);
+                }
+            }
+            foreach (var item in tipusok) Console.WriteLine($"\t{item}");
+
+            //orvosi.txt néven egy utf-8 kódolású fájl --> tartalmazza az összes kiosztott orvosi nobel-díj adatait
             Console.ReadKey();
         }
     }
