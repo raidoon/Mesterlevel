0ff1f6b538920d89d8c63c486dc0dd151cac3112
raidoon <thomaselphin@gmail.com>
2024-09-17 11:42:14 +0200

    urlap.html + urlap.js

--- HTML +CSS + JavaScript/javascript_feladatok/urlap.js
+++ HTML +CSS + JavaScript/javascript_feladatok/urlap.js
@@ -1,13 +1,30 @@
 /*Ûrlapok-Weblapon(design nélkül)
 -urlap.html fájlba dolgozz!
 
-2. Kérd be termék nevét, árát, készeleten lévõ darabszámát, 
-lenyílóban jelenjen meg, hogy tejtermék/pékáru/édesség
-Gombnyomásra írja vissza a gomb alá a termék nevét, árát, darabszámát, típusát,
-majd alá az összértéket (ár*darab)
-A termék neve beviteli mezõjének háttere legyen sárga, ha belelépünk, 
-és fehér, ha kilépünk. */
+2. Kérd be termék nevét, árát, készeleten lévõ darabszámát, lenyílóban jelenjen meg, hogy tejtermék/pékáru/édesség
+
+A termék neve beviteli mezõjének háttere legyen sárga, ha belelépünk, és fehér, ha kilépünk. */
+
+function kiSzinezes(elem){
+    elem.style.backgroundColor="yellow"
+}
+
+function visszaSzinez(elem){
+    elem.style.backgroundColor="white"
+}
 
 function kattintas(){
-    
+    console.log("a kattintás függvény lefutott!")
+    var kiiratos_div = document.getElementById("kiiratos_div")
+    var termek_neve = document.getElementById("termek_neve").value
+    var termek_ara = document.getElementById("termek_ara").value
+    var termek_tipusa = document.getElementById("termek_tipusa").value
+    var termek_db = document.getElementById("termek_db").value
+
+    //Gombnyomásra írja vissza a gomb alá a termék nevét, árát, darabszámát, típusát, majd alá az összértéket (ár*darab)
+    kiiratos_div.innerHTML=`
+    <br> Termék neve: ${termek_neve} <br> Termék ára: ${termek_ara} Ft
+    <br> Termék darabszáma: ${termek_db} db
+    <br> Termék típusa: ${termek_tipusa}
+    <br> Termék összértéke: ${termek_ara*termek_db} Ft`
 }
