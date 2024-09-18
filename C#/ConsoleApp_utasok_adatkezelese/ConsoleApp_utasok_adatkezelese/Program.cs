5522a6e62ad6b2e201414db4c05f92e3101e6390
raidoon <thomaselphin@gmail.com>
2024-09-18 13:03:57 +0200

    console app csv kezeles

---
+++ C#/ConsoleApp_utasok_adatkezelese/ConsoleApp_utasok_adatkezelese/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_utasok_adatkezelese
+{
+    internal class Program
+    {
+        public static List<adatok> adatok = new List<adatok>();
+        static void Main(string[] args)
+        {
+            string[] fajl = File.ReadAllLines("utasadat.txt", Encoding.UTF8);
+            foreach (string f in fajl) adatok.Add(new adatok(f));
+            /*foreach (var i in adatok) {Console.WriteLine($"{i.MegalloSorszam,-5}{i.FelszallasDatumIdo,-15}{i.KartyaAzon,-10}
+                {i.BerletTipus,-5}{i.ErvenyessegiDatum,-10}{i.ervenyessegEllenorzes()}")};*/ // ---> teszt
+            
+            Console.WriteLine($"Végállomástól végállomásig összesen {adatok.Count()} utas szeretett volna felszállni a buszra.");
+
+            //hány esetben kellett a buszvezetőnek elutasítania az utas felszállását?
+            int ervenytelenJegyekSzama = 0;
+            foreach (var i in adatok) if (!i.ervenyessegEllenorzes()) ervenytelenJegyekSzama++;
+            Console.WriteLine($"A buszvezetőnek {ervenytelenJegyekSzama} alkalommal kellett elutasítania az utas felszállását.");
+
+            //Melyik megállóban próbált meg felszállni a legtöbb utas?
+            var maxUtasSzam = adatok.GroupBy(x => x.MegalloSorszam).OrderByDescending(x => x.Count()).FirstOrDefault();
+            if(maxUtasSzam!=null)  Console.WriteLine($"A {maxUtasSzam.Key}. megállóban próbált meg felszállni a legtöbb utas ({maxUtasSzam.Count()} fő).");
+
+            //Hány ingyenes és kedvezményes bérlet volt?
+            int ingyenesDB = 0, kedvezmenyesDB = 0;
+            foreach (var i in adatok) if (i.ervenyessegEllenorzes())
+            {
+                if (i.kedvezmenyes()) kedvezmenyesDB++;
+                if (i.ingyenes()) ingyenesDB++;
+            }
+            Console.WriteLine($"Ingyenesen utazók száma: {ingyenesDB} fő" +
+                $"\nKedvezményesen utazók száma: {kedvezmenyesDB} fő");
+
+
+            /*Függvény napokszama(e1:egész, h1:egész, n1: egész, e2:egész, h2: egész, n2: egész): egész
+	            h1 = (h1 + 9) MOD 12s
+	            e1 = e1 - h1 DIV 10
+	            d1= 365*e1 + e1 DIV 4 - e1 DIV 100 + e1 DIV 400 + (h1*306 + 5) DIV 10 + n1 - 1
+	            h2 = (h2 + 9) MOD 12
+	            e2 = e2 - h2 DIV 10
+	            d2= 365*e2 + e2 DIV 4 - e2 DIV 100 + e2 DIV 400 + (h2*306 + 5) DIV 10 + n2 - 1
+	            napokszama:= d2-d1
+            Függvény vége*/
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C#/ConsoleApp_utasok_adatkezelese/ConsoleApp_utasok_adatkezelese/adatok.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_utasok_adatkezelese
+{
+    internal class adatok
+    {
+        //0 20190326-0700 4170861 NYB 20190404
+        //0 20190326-0700 6915170 FEB 20210101
+        public int MegalloSorszam { get; set; }
+        public string FelszallasDatumIdo { get; set; } //ééééhhnn-óópp
+        public string KartyaAzon { get; set; } //nem számolunk vele, úgyhogy maradhat string
+        public string BerletTipus { get; set; }
+        public string ErvenyessegiDatum {get;set;}
+
+        public adatok(string sor)
+        {
+            string[] n = sor.Split(' ');
+            MegalloSorszam = int.Parse(n[0]);
+            FelszallasDatumIdo = n[1];
+            KartyaAzon = n[2];
+            BerletTipus = n[3];
+            ErvenyessegiDatum = n[4];
+        }
+        //csak érvényes jeggyel vagy bérlettel lehet utazni
+        public bool ervenyessegEllenorzes()
+        {
+            if (ErvenyessegiDatum.Length <= 2) return ErvenyessegiDatum != "0";
+            else
+            {
+                DateTime felszallasDatuma = DateTime.ParseExact(FelszallasDatumIdo.Split('-')[0], "yyyyMMdd", CultureInfo.InvariantCulture);
+                DateTime ervenyessegDatuma = DateTime.ParseExact(ErvenyessegiDatum, "yyyyMMdd", CultureInfo.InvariantCulture);
+                return ervenyessegDatuma>=felszallasDatuma;
+            }
+        }
+
+        //bérletekre függvény
+        public bool kedvezmenyes()
+        {
+            if (BerletTipus == "TAB" || BerletTipus == "NYB") return true;
+            else return false;
+        }
+        public bool ingyenes()
+        {
+            if (BerletTipus == "NYP" || BerletTipus == "RVS" || BerletTipus=="GYK") return true;
+            else return false;
+        }
+    }
+}
