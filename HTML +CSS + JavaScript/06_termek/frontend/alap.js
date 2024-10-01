6efddab9ea25be7717c5ff56619cb6b9095c00ef
raidoon <thomaselphin@gmail.com>
2024-10-01 11:23:30 +0200

    frontend update

--- HTML +CSS + JavaScript/06_termek/frontend/alap.js
+++ HTML +CSS + JavaScript/06_termek/frontend/alap.js
@@ -1,7 +1,4 @@
-/*termékek neve p-kben || új termék felvitele, formmal, gombbal, mindennel*/
-
-/*
-fetch('http://localhost:3000/termekLekerdez')
+/*fetch('http://localhost:3000/termekLekerdez')
 .then(x=>x.json())
 .then(y=>megjelenites(y))
 
@@ -12,28 +9,12 @@ function megjelenites(y){
     }
 } 
 */
-
 termekLekerdez() //------------------------elindítjuk az indítás functiont
-tipusLekerdez()
-
 async function termekLekerdez() {
     let x = await fetch('http://localhost:3000/termekLekerdez');
     let y = await x.json();
     termekMegjelenites(y);
 }
-
-async function tipusLekerdez() {
-    let x = await fetch('http://localhost:3000/tipusLekerdez');
-    let y = await x.json();
-    tipusMegjelenites(y);
-}
-//-------------------------------------------- lenyilo listába tesszük a típusokat
-function tipusMegjelenites(y){
-    for (const elem of y) {
-        document.getElementById("lenyilo").innerHTML+= `<option value="${elem.tipus_id}">${elem.tipus_nev}</option>`
-    }
-}
-
 //-------------------------------------------- lekérdezzük a termékeket
 function termekMegjelenites(y){
     console.log(y)
@@ -56,5 +37,34 @@ function tipusFelvitel(){
     .then(x=> x.text())
     .then(y=> {
         document.getElementById("visszajelzes").innerHTML = y
+    });
+}
+//-------------------------------------------- lenyilo listába tesszük a típusokat
+tipusLekerdez()
+async function tipusLekerdez() {
+    let x = await fetch('http://localhost:3000/tipusLekerdez');
+    let y = await x.json();
+    tipusMegjelenites(y);
+}
+function tipusMegjelenites(y){
+    for (const elem of y) {
+        document.getElementById("lenyilo").innerHTML+= `<option value="${elem.tipus_id}">${elem.tipus_nev}</option>`
+    }
+}
+//-------------------------------------------- új termék felvitele az adatbázisba
+function TermekFelvitel(){
+    var adatok = {
+        "bevitel1" : document.getElementById("ujTermek_neve").value,
+        "bevitel2" : document.getElementsById("ujTermek_ara").value,
+        "bevitel3" : document.getElementsById("lenyilo").value
+    }
+    fetch("http://localhost:3000/termekFelvitel",{
+        method:"POST",
+        body: JSON.stringify(adatok),
+        headers:{"Content-type":"application/json; charset=UTF-8"}
+    })
+    .then(x=> x.text())
+    .then(y=> {
+        document.getElementById("visszajelzes2").innerHTML = y
     });
 }
