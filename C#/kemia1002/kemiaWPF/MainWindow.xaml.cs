dc368fd51e567343a077dc480e1c0246ec6f23a8
raidoon <thomaselphin@gmail.com>
2024-10-02 14:40:05 +0200

    update kemia1002

--- C#/kemia1002/kemiaWPF/MainWindow.xaml.cs
+++ C#/kemia1002/kemiaWPF/MainWindow.xaml.cs
@@ -34,7 +34,10 @@ namespace kemiaWPF
         private void adatokbetoltese()
         {
             adatok.Clear();
+            cb_ev.Items.Clear();
             adatok = Backend.GET(url).Send().As<List<Adatsor>>();
+            for (int i = DateTime.Now.Year; i >= 1900; i--) cb_ev.Items.Add(i);
+            cb_ev.SelectedIndex = 0;
             datagrid.ItemsSource = adatok;
         }
 
@@ -52,6 +55,41 @@ namespace kemiaWPF
                 datagrid.ItemsSource=keresettLista;
             }
             else datagrid.ItemsSource = adatok;
+        }
+
+        private void bt_adatrogzites_Click(object sender, RoutedEventArgs e)
+        {
+            lb_uzenet.Content = "";
+            if (tb_nev.Text != null && tb_rendszam.Text != null && tb_vegyjel.Text != null)
+            {
+                if (tb_felfedezo.Text == "") tb_felfedezo.Text = "Ismeretlen";
+                Nagykezdobetus(tb_nev);
+                Nagykezdobetus(tb_vegyjel);
+            }
+            else lb_uzenet.Content = "A felfedezőn kívül mindent kötelező megadni!";
+        }
+
+        private void tb_rendszam_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string szamok = "0123456789"; //érvényes karakterek
+            string csakSzamok = "";//instant törölölni fogom, ha valaki nem számot ír be
+            string begepeltErtek = tb_rendszam.Text;
+            for (int i = 0; i < begepeltErtek.Length; i++)
+            {
+                if (szamok.Contains(begepeltErtek[i]) && csakSzamok.Length <= 3) csakSzamok += begepeltErtek[i];
+            }
+            tb_rendszam.Text = csakSzamok;
+        }
+        private void Nagykezdobetus(TextBox textbox)
+        {
+            string szoveg =textbox.Text;
+            string ujSzoveg = "";
+            for (int i = 0; i < szoveg.Length; i++)
+            {
+                if (i == 0) ujSzoveg += szoveg[i].ToString().ToUpper();
+                else ujSzoveg += szoveg[i].ToString().ToLower();
+                textbox.Text = ujSzoveg;
+            }
         }
     }
 }
