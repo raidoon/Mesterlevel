a2dc84281fa486df294551992c80192d92760f5e
raidoon <thomaselphin@gmail.com>
2024-10-17 11:42:51 +0200

    starting front end

--- HTML +CSS + JavaScript + React + React Native/18_marvel_szavazas/frontend/Marvel/App.js
+++ HTML +CSS + JavaScript + React + React Native/18_marvel_szavazas/frontend/Marvel/App.js
@@ -1,11 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
+  const [adatok,setAdatok] = useState([])
+  const letoltes=async()=>{
+    const x = await fetch("http:192.168.10.57:3000/film")
+    const y = await x.json()
+    setAdatok(y)
+    //alert(JSON.stringify(y))
+  }
+
+  useEffect(()=>{
+    letoltes()
+  },[])
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Marvel filmek</Text>
+      <Image source={{uri: 'http:192.168.10.57:3000/kep1.jpg'}} style={{height: 500, width: 300, marginTop: 50}}/>
     </View>
   );
 }
@@ -15,6 +26,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 50
   },
 });
