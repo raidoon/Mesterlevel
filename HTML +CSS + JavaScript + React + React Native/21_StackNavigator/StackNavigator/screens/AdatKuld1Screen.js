9e5a7397fda2c51da310d5c480e281cb495ef7dc
raidoon <thomaselphin@gmail.com>
2024-11-11 10:07:08 +0100

    adat visszaküldése a home screenre

--- HTML +CSS + JavaScript + React + React Native/21_StackNavigator/StackNavigator/screens/AdatKuld1Screen.js
+++ HTML +CSS + JavaScript + React + React Native/21_StackNavigator/StackNavigator/screens/AdatKuld1Screen.js
@@ -1,15 +1,24 @@
 import React from 'react';
-import { View, Text, Button } from 'react-native';
+import { useState } from 'react';
+import { View, Text, Button, TextInput } from 'react-native';
 
 export default function AdatKuld1Screen({navigation,route}) {
   const {atkuld} = route.params
+  const [szoveg,setSzoveg] = useState("")
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Adat küldés egyik screenről a másikra 1.</Text>
       <Text>{atkuld}</Text>
+      <TextInput value={szoveg} 
+            onChangeText={setSzoveg} 
+            style={{
+                backgroundColor:"grey",
+                width:250,
+                margin: 30,
+                color:'white'}}/>
       <Button
         title="Adat küldés 2 képernyő meghívása"
-        onPress={() => navigation.navigate('AdatKuld2')}
+        onPress={() => navigation.navigate('AdatKuld2',{atkuld:szoveg})}
       />
     </View>
   );
--- HTML +CSS + JavaScript + React + React Native/21_StackNavigator/StackNavigator/screens/AdatKuld2Screen.js
+++ HTML +CSS + JavaScript + React + React Native/21_StackNavigator/StackNavigator/screens/AdatKuld2Screen.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { View, Text, Button} from 'react-native';
 
-export default function AdatKuld2Screen({navigation}) {
+export default function AdatKuld2Screen({navigation,route}) {
+    const {atkuld} = route.params
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Adat küldés 2. képernyő</Text>
+      <Text style={{marginBottom:30}}>Adat küldés 2. képernyő</Text>
+      <Text>{atkuld}</Text>
     </View>
   );
 }
