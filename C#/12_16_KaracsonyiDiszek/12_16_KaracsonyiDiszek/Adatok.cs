65f6c3209b0e6e1d0a885199e2062e1e86fa318a
raidoon <thomaselphin@gmail.com>
2024-12-16 12:21:21 +0100

    Karacsonyi Diszek console app kesz

---
+++ C#/12_16_KaracsonyiDiszek/12_16_KaracsonyiDiszek/Adatok.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12_16_KaracsonyiDiszek
+{
+    public class Adatok
+    {
+        /* 1;3;0;0;0;4;0 
+           2;4;0;5;-2;1;0 */
+        //Az elkészített díszek száma mindig pozitív vagy 0 értékű, míg az eladott díszek száma mindig negatív vagy 0 értékű
+        public int nap {  get; set; }
+        public int harang { get; set; }
+        public int harangEladas { get; set; }
+        public int angyalka { get; set; }
+        public int angyalkaEladas { get; set; }
+        public int fenyofa { get; set; }
+        public int fenyofaEladas { get; set; } //e 6 harang, 4 angyalka és 5 fenyőfa
+        public double NapiBevetel()
+        {
+            const double harangAr = 1000;
+            const double angyalkaAr = 1350;
+            const double fenyoFaAr = 1500;
+
+            // Bevétel: eladott díszek ára
+            double bevetelek = 0;
+
+            bevetelek += harangEladas * harangAr;
+            bevetelek += angyalkaEladas * angyalkaAr;
+            bevetelek += fenyofaEladas * fenyoFaAr;
+
+            return bevetelek;
+        }
+        public Adatok (string sor)
+        {
+            string[] s = sor.Split(';');
+            nap = int.Parse(s[0]);
+            harang = int.Parse(s[1]);
+            harangEladas = int.Parse(s[2]);
+            angyalka = int.Parse(s[3]);
+            angyalkaEladas = int.Parse(s[4]);
+            fenyofa = int.Parse(s[5]);
+            fenyofaEladas = int.Parse(s[6]);
+        }
+    }
+}
---
+++ C#/12_16_KaracsonyiDiszek/12_16_KaracsonyiDiszek/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12_16_KaracsonyiDiszek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int diszDb = 0, szam = 0, harang = 0, fenyofa = 0, angyalka = 0;
+            bool nemCsinaltDiszt = false, okszam = false;
+            List<Adatok> adatok = new List<Adatok>();
+            string[] file = File.ReadAllLines("diszek.txt");
+            foreach (var i in file) adatok.Add(new Adatok(i));
+            Console.WriteLine($"Adatok száma a fájlban: {adatok.Count()}");
+            foreach (var i in adatok) diszDb += i.harang + i.angyalka + i.fenyofa;
+            Console.WriteLine($"4.feladat: Összesen {diszDb} db dísz készült.");
+            foreach(var i in adatok)
+            {
+                while (!nemCsinaltDiszt)
+                {
+                    if (i.harang == 0 && i.fenyofa == 0 && i.angyalka == 0)
+                    {
+                        nemCsinaltDiszt = true;
+                        Console.WriteLine($"\n5.feladat: Volt olyan nap, amikor egyetlen dísz sem készült."); break;
+                    }
+                    break;
+                }
+            }
+            Console.WriteLine("\n6.feladat:");
+            while (!okszam)
+            {
+                Console.Write("Adja meg a keresett napot [1 ... 40]: ");
+                try
+                {
+                    szam = int.Parse(Console.ReadLine());
+                    if (szam <= 40 && szam >= 1) okszam = true;
+                    else okszam = false;
+
+                }catch(Exception e) { Console.WriteLine(e.Message); }
+            }
+            foreach (var i in adatok)
+            {
+                if (i.nap <= szam) 
+                {
+                    harang += i.harang;
+                    harang += i.harangEladas;
+
+                    angyalka += i.angyalka;
+                    angyalka += i.angyalkaEladas;
+                    
+                    fenyofa += i.fenyofa;
+                    fenyofa += i.fenyofaEladas;
+                }
+            }
+            Console.WriteLine($"\tA(z) {szam}. nap végén {harang} harang, {angyalka} angyalka és {fenyofa} fenyőfa maradt készleten.");
+            harang = 0; angyalka = 0; fenyofa = 0;
+            foreach (var item in adatok) { harang -= item.harangEladas; angyalka -= item.angyalkaEladas; fenyofa -= item.fenyofaEladas; }
+            if (angyalka > harang && angyalka > fenyofa) Console.WriteLine($"\n7.feladat: Legtöbbet eladott dísz: {angyalka} darab\n\tAngyalka");
+            else if (harang > angyalka && harang > fenyofa) Console.WriteLine($"\n7.feladat: Legtöbbet eladott dísz: {harang} darab\n\tHarang");
+            else if (fenyofa > angyalka && fenyofa > harang) Console.WriteLine($"\n7.feladat: Legtöbbet eladott dísz: {fenyofa} darab\n\tFenyőfa");
+            else if (angyalka > harang && angyalka == fenyofa) Console.WriteLine($"\n7.feladat: Legtöbbet eladott dísz: {angyalka} darab\n\tAngyalka\n\tFenyőfa");
+            else if (angyalka == harang && angyalka == fenyofa) Console.WriteLine($"\n7.feladat: Legtöbbet eladott dísz: {harang} darab\n\tHarang\n\tAngyalka\n\tFenyőfa");
+            else if (harang == angyalka && harang > fenyofa) Console.WriteLine($"\n7.feladat: Legtöbbet eladott dísz: {harang} darab\n\tHarang\n\tAngyalka");
+            else if (harang > angyalka && harang == fenyofa) Console.WriteLine($"\n7.feladat: Legtöbbet eladott dísz: {harang} darab\n\tHarang\n\tFenyőfa");
+
+            Console.ReadKey();
+        }
+    }
+}
