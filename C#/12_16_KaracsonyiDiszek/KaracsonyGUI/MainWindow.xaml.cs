1650e8b28e90c14d612aa50ea6588642bb2661b1
raidoon <thomaselphin@gmail.com>
2025-01-06 13:06:23 +0100

    GUI

--- C#/12_16_KaracsonyiDiszek/KaracsonyGUI/MainWindow.xaml.cs
+++ C#/12_16_KaracsonyiDiszek/KaracsonyGUI/MainWindow.xaml.cs
@@ -12,17 +12,64 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+using NetworkHelper;
+using karacsonyCLI;
 namespace KaracsonyGUI
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        List<Adatsor> adatok = new List<Adatsor>();
+        public static int nap = 0, 
+            HarangKeszOsszesen = 0, AngyalKeszOsszesen = 0, FenyoKeszOsszesen = 0, 
+            HarangEladottOsszesen = 0, AngyalEladottOsszesen = 0, FenyoEladottOsszesen = 0, 
+            HarangKeszletOsszesen = 0, FenyoKeszletOsszesen = 0, AngyalKeszletOsszesen = 0;
+        public static bool HarangOK = false, AngyalOK = false, FenyoOK = false
         public MainWindow()
         {
             InitializeComponent();
+            adatokbetoltese();
+        }
+
+        private void adatokbetoltese()
+        {
+            adatok.Clear();
+            listbox.Items.Clear();
+            string url = "http://localhost:3000/ujdiszeklista";
+            adatok = Backend.GET(url).Send().As<List<Adatsor>>();
+            if (adatok.Count != 0)
+            {
+                HarangKeszOsszesen = 0;
+                HarangEladottOsszesen = 0;
+                HarangKeszletOsszesen = 0; 
+                AngyalKeszOsszesen = 0;
+                AngyalKeszletOsszesen = 0;
+                AngyalEladottOsszesen = 0; 
+                FenyoEladottOsszesen = 0;
+                FenyoKeszletOsszesen = 0;
+                FenyoKeszOsszesen = 0;
+                List<string> lista = new List<string>();
+                foreach(var a in adatok)
+                {
+                    HarangKeszOsszesen += a.keszharang;
+                    HarangEladottOsszesen += a.eladottharang;
+                    HarangKeszletOsszesen = HarangKeszOsszesen + HarangEladottOsszesen; 
+                    
+                    AngyalKeszOsszesen += a.keszangyal;
+                    AngyalEladottOsszesen += a.eladottangyal;
+                    AngyalKeszletOsszesen = AngyalKeszletOsszesen + AngyalEladottOsszesen;
+
+                    FenyoKeszOsszesen += a.keszfenyo;
+                    FenyoEladottOsszesen += a.eladottfenyo;
+                    FenyoKeszletOsszesen = FenyoKeszOsszesen + FenyoEladottOsszesen;
+                    lista.Add($"{a.nap,-5} nap");
+                    
+                }
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
         }
     }
 }
