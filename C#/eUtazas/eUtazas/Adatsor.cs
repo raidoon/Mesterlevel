26f07cb117fc7a709cd30ef2c6b00d3839634137
raidoon <thomaselphin@gmail.com>
2025-03-27 10:52:19 +0100

    eUtazas

---
+++ C#/eUtazas/eUtazas/Adatsor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eUtazas
+{
+    internal class Adatsor
+    {
+        /*0 20190326-0700 4170861 NYB 20190404
+         0 20190326-0700 6915170 FEB 20210101
+         0 20190326-0700 4953658 FEB 20210101
+         0 20190326-0700 3073247 FEB 20190413*/
+        public int megalloSorszama {  get; set; }
+        public int felszallasDatuma { get; set; }
+        public int felszallasOraPerc {  get; set; }
+        public int cardAzon {  get; set; }
+        public string cardTipus {  get; set; }
+        public int cardFelhasznalhato { get; set; }
+        public int vanJegye {  get; set; }
+        public Adatsor(string sor)
+        {
+            string[] n = sor.Split(' ');
+            megalloSorszama = int.Parse(n[0]);
+            felszallasDatuma = int.Parse(n[1].Split('-')[0]);
+            felszallasOraPerc = int.Parse(n[1].Split('-')[1]);
+            cardAzon = int.Parse(n[2]);
+            cardTipus = n[3];
+            cardFelhasznalhato = int.Parse(n[4]);
+            //20190326-20210101
+            if (cardFelhasznalhato.ToString().Length==8) vanJegye = 0; else vanJegye = 1;
+        }
+    }
+}
---
+++ C#/eUtazas/eUtazas/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace eUtazas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Adatsor> adatok = new List<Adatsor>();
+            string[] file = File.ReadAllLines("utasadat.txt", Encoding.UTF8);
+            foreach (string sor in file) adatok.Add(new Adatsor(sor));
+            Console.WriteLine($"2. feladat\nA buszra {adatok.Count} utas akart felszállni.");
+            var noValidTicket = adatok
+                                    .Where(x => (x.vanJegye == 0 && x.felszallasDatuma > x.cardFelhasznalhato)
+                                             || (x.vanJegye == 1 && x.cardFelhasznalhato == 0))
+                                    .ToList();
+            Console.WriteLine($"\n3. feladat\nA buszra {noValidTicket.Count()} utas nem szállhatott fel.");
+            var legtobb = adatok.GroupBy(x => x.megalloSorszama)
+                                .OrderByDescending(x => x.Key)
+                                .OrderByDescending (x => x.Count())
+                                .ThenBy(x => x.Key)
+                                .ToList();
+            Console.WriteLine($"\n4. feladat\nA legtöbb utas ({legtobb.First().Count()} fő) a {legtobb.First().Key}. megállóban próbált felszállni.");
+            var kedvezmenyes = adatok
+                 .Where(x => ((x.vanJegye == 0 && x.felszallasDatuma <= x.cardFelhasznalhato) && (x.cardTipus == "TAB" || x.cardTipus == "NYB"))
+                          || ((x.vanJegye == 1 && x.cardFelhasznalhato > 0) && (x.cardTipus == "TAB" || x.cardTipus == "NYB"))
+                 ).ToList();
+            Console.WriteLine($"\n5.fealdat\nIngyenesen utazók száma: " +
+                $"{adatok.Where(x=> x.cardTipus=="NYP"||x.cardTipus=="RVS"||x.cardTipus=="GYK").ToList().Count} fő" +
+                $"\nKedvezményesen utazók száma: {kedvezmenyes.Count} fő");
+            Console.ReadKey();
+        }
+    }
+}
